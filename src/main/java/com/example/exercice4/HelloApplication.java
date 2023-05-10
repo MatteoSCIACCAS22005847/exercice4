46a355ddbc0cd4e753ec1e5ef6e1f520bb6a50b9
Matteo <matteo.sciacca@etu.univ-amu.fr>
2023-05-10 12:13:37 +0200

    ex4

--- src/main/java/com/example/exercice4/HelloApplication.java
+++ src/main/java/com/example/exercice4/HelloApplication.java
@@ -3,26 +3,47 @@ package com.example.exercice4;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.geometry.Pos;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
 
         BorderPane borderPane = new BorderPane();
-        TextField text1 = new TextField("Couleur choisie x fois");
-        borderPane.setTop(text1);
+        Label text1 = new Label("Couleur choisie x fois");
+        HBox hb1 = new HBox();
+        //borderPane.setTop(text1);
+        //borderPane.setBottom(hb1);
+        text1.setAlignment(Pos.TOP_CENTER);
+        hb1.setAlignment(Pos.BOTTOM_CENTER);
+        Button b1 = new Button("Vert");
+        Button b2 = new Button("Rouge");
+        Button b3 = new Button("Bleu");
+        hb1.getChildren().add(b1);
+        hb1.getChildren().add(b2);
+        hb1.getChildren().add(b3);
+
+        // Ajout du conteneur à la scene
+        Scene scene = new Scene(borderPane);
+
+        // Ajout de la scene à la fenêtre et changement de ses paramètres (dimensions et titre)
+        stage.setScene(scene);
+        stage.setWidth(400);
+        stage.setHeight(200);
+        stage.setTitle("Ex4");
+
+        // Affichage de la fenêtre
+        stage.show();
 
     }
 
@@ -30,3 +51,4 @@ public class HelloApplication extends Application {
         launch();
     }
 }
+
